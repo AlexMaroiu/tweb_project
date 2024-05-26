c5827a70db2052098cdad167b0bc064c0ec5471d
Iordan Robert <iordan.robert.valentin@gmail.com>
2024-05-26 22:23:10 +0300

    github oauth implementation

--- auth.js
+++ auth.js
@@ -1,22 +1,39 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const passport = require("passport");
 
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
+const GithubStrategy = require("passport-github").Strategy;
 
-passport.use(new GoogleStrategy({
-        clientID: GOOGLE_CLIENT_ID,
-        clientSecret: GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:8080/google/callback",
-        passReqToCallback: true,
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:8080/google/callback",
+      passReqToCallback: true,
     },
-    function(request, accessToken, refreshToken, profile, done) {
-        return done(null, profile);
-    })
+    function (request, accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
 );
 
-passport.serializeUser(function(user, done) {
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: "http://localhost:8080/github/callback",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      return cb(null, profile);
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
+passport.deserializeUser(function (user, done) {
   done(null, user);
 });
--- index.js
+++ index.js
@@ -1,79 +1,90 @@
-const express = require('express');
-const session = require('express-session');
-const swaggerJSDOC = require('swagger-jsdoc');
-const swaggerUI = require('swagger-ui-express');
-const passport = require('passport');
-require('./auth');
+const express = require("express");
+const session = require("express-session");
+const swaggerJSDOC = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+const passport = require("passport");
+require("./auth");
 
 const app = express();
 const PORT = 8080;
 
-
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
+passport.deserializeUser(function (user, done) {
   done(null, user);
 });
 
-app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-
 const options = {
-    definition:{
-        openapi : '3.0.0',
-        info : {
-            title : 'Node JS API project for oAuth and mongoDB',
-            version: '1.0.0'
-        },
-        servers: [
-            { url:  '/localhost:8080' },
-        ]
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Node JS API project for oAuth and mongoDB",
+      version: "1.0.0",
     },
-    apis: ['./index.js']
-}
+    servers: [{ url: "/localhost:8080" }],
+  },
+  apis: ["./index.js"],
+};
 
 const swaggerSpec = swaggerJSDOC(options);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
-
-app.listen(
-    PORT,
-    () => console.log(`its alive on http://localhost:${PORT}`)
-);
+app.listen(PORT, () => console.log(`its alive on http://localhost:${PORT}`));
 
 function isLoggedIn(req, res, next) {
-    req.user ? next() : res.sendStatus(401);
+  req.user ? next() : res.sendStatus(401);
 }
 
-
-
-app.get('/', (req, res) => {
-    res.send('<a href="/auth/google">Authenticate with Google</a>');
+app.get("/", (req, res) => {
+  res.send(`
+    <a href="/auth/google">Authenticate with Google</a>
+    <a href="/auth/github">Authenticate with Github</a>
+    `);
 });
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope: [ 'email', 'profile' ] }
-));
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
 
-app.get( '/google/callback',
-  passport.authenticate( 'google', {
-    successRedirect: '/protected',
-    failureRedirect: '/auth/google/failure'
+app.get(
+  "/auth/github",
+  passport.authenticate("github", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/protected/google-user",
+    failureRedirect: "/auth/google/failure",
   })
 );
 
-app.get('/protected', isLoggedIn, (req, res) => {
-    res.send(`Hello ${req.user.displayName}`);
+app.get(
+  "/github/callback",
+  passport.authenticate("github", {
+    successRedirect: "/protected/github-user",
+    failureRedirect: "/auth/google/failure",
+  })
+);
+
+app.get("/protected/google-user", isLoggedIn, (req, res) => {
+  res.send(`Hello ${req.user.displayName}`);
 });
-  
-app.get('/auth/google/failure', (req, res) => {
-    res.send('Failed to authenticate..');
+
+app.get("/protected/github-user", isLoggedIn, (req, res) => {
+  res.send(`Hello ${req.user.username}`);
+});
+
+app.get("/auth/google/failure", (req, res) => {
+  res.send("Failed to authenticate..");
 });
 
 /**
@@ -104,4 +115,3 @@ app.get('/auth/google/failure', (req, res) => {
 //         thsirt: `shirt with your logo ${logo} and ID of ${id}`
 //     });
 // });
-
